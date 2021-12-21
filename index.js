b3bd7cb71e119e033eecb657fd6b00fedd75d6d6
opai elsheikh <72025112+opaielsheikh@users.noreply.github.com>
2021-12-21 13:51:16 +0000

    assets

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-export { default as ChannelContainer } from './ChannelContainer';
-export { default as ChannelListContainer } from './ChannelListContainer';
-export { default as ChannelSearch } from './ChannelSearch';
-export { default as TeamChannelList } from './TeamChannelList';
-export { default as TeamChannelPreview } from './TeamChannelPreview';
-export { default as ChannelInner } from './ChannelInner';
-export { default as CreateChannel } from './CreateChannel';
-export { default as EditChannel } from './EditChannel';
-export { default as TeamMessage } from './TeamMessage';
-export { default as Auth } from './Auth';
-export { default as UserList } from './UserList';
-export { default as ResultsDropdown } from './ResultsDropdown';
+export { AddChannel } from './AddChannel';
+export { ChannelInfo } from './ChannelInfo';
+export { CloseCreateChannel } from './CloseCreateChannel';
+export { InviteIcon } from './InviteIcon';
+export { LightningBolt } from './LightningBolt';
+export { SearchIcon } from './SearchIcon';
