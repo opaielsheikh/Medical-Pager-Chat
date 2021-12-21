38df06f8d5bf8b678f38c2a333ddec405a09d487
opai elsheikh <72025112+opaielsheikh@users.noreply.github.com>
2021-12-21 13:50:16 +0000

    components

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-import App from './App';
-
-ReactDom.render(<App />, document.getElementById('root'));
+export { default as ChannelContainer } from './ChannelContainer';
+export { default as ChannelListContainer } from './ChannelListContainer';
+export { default as ChannelSearch } from './ChannelSearch';
+export { default as TeamChannelList } from './TeamChannelList';
+export { default as TeamChannelPreview } from './TeamChannelPreview';
+export { default as ChannelInner } from './ChannelInner';
+export { default as CreateChannel } from './CreateChannel';
+export { default as EditChannel } from './EditChannel';
+export { default as TeamMessage } from './TeamMessage';
+export { default as Auth } from './Auth';
+export { default as UserList } from './UserList';
+export { default as ResultsDropdown } from './ResultsDropdown';
